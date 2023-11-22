1976019b66cb648f372a75baba4366de7144c39c
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-11-22 15:06:39 +0300

    add-to-basket

--- app.js
+++ app.js
@@ -14,19 +14,27 @@ function ekranaYazdir(urunler) {
         card.classList.add("card", "p-3")
         card.innerHTML =
             `
-        <img src="${urun.thumbnail}" style="width: 18rem;" class="card-img-top m-auto" alt="..." />
+        <img src="${urun.thumbnail}" style="width: 18rem;" class="card-img-top resim m-auto" alt="..." />
         <div class="card-body text-center">
-          <h5 class="card-title">${urun.title}</h5>
+          <h5 class="card-title ad">${urun.title}</h5>
           <p class="card-text">
             ${urun.description}
           </p>
-          <p class="card-text">
-            ${urun.price}
+          <p class="card-text fiyat">
+            ${urun.price}₺
           </p>
-         
-          <a href="#" class="btn btn-primary">Satın Al</a>
+          <button class="btn btn-primary tikla">Satın Al</button>
         </div>
         `
         wrapper.append(card)
+    })
+    const buttonlar = document.querySelectorAll(".tikla")
+    buttonlar.forEach(btn => {
+        btn.addEventListener("click", function () {
+            let card = this.parentElement.parentElement
+            let isim = card.querySelector(".isim").textContent
+            let resim = card.querySelector(".resim").src
+            let fiyat = card.querySelector(".fiyat").textContent
+        })
     })
 }
